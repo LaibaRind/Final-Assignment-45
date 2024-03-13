293225f0c84840855fdf05f647a3b2eb22fa9759
unknown <laibanwer394@gmail.com>
2024-03-13 13:24:45 +0500

    first commit

---
+++ Assignment 1/main.js
@@ -0,0 +1,3 @@
+//Assignment # 1
+var personname = "laiba"; //Storing the person name in variable
+console.log("Hello ".concat(personname, " would you like to learn typescript today?")); //printing a message to the person
---
+++ Assignment 1/main.ts
@@ -0,0 +1,5 @@
+//Assignment # 1
+
+let personname :string  = "laiba";     //Storing the person name in variable
+
+console.log(`Hello ${personname} would you like to learn typescript today?`)    //printing a message to the person
